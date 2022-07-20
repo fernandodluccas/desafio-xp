2e12f2e4f3ec507fa091b3b7da53e49d29a47f4b
Fernando D'luccas <dluccash@hotmail.com>
2022-07-20 18:49:55 -0300

    feat(server.ts): implements a basic route

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.json({ message: 'Hello World!' }));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+export default app;
