d293ec79ac6315f6a59663598680f275e8062190
Fernando D'luccas <dluccash@hotmail.com>
2022-07-20 21:36:18 -0300

    feat(authentication): implements authentication control

---
+++ src/controllers/authenticate.customer.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import authenticateCustomerService from '../services/authenticate.customer.service';
+
+class AuthenticateCustomerController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { email, password } = req.body;
+
+    const token = await authenticateCustomerService.execute({
+      email,
+      password,
+    });
+
+    return res.status(201).json({ token });
+  }
+}
+
+export default new AuthenticateCustomerController();
