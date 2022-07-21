01151d3225a4ae0f4fdf16992ef3e0a2ac89d7bd
Fernando D'luccas <dluccash@hotmail.com>
2022-07-20 23:09:28 -0300

    feat(deposit): implements deposit route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authenticateRoutes from './authenticate.routes';
 import customerRoutes from './customers.routes';
+import transactionsRoutes from './transactions.routes';
 
 const routes = Router();
 
 routes.use('/customers', customerRoutes);
 routes.use('/authenticate', authenticateRoutes);
+routes.use('/account', transactionsRoutes);
 
 export default routes;
--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import depositTransactionController from '../controllers/deposit.transaction';
 
 const transactionsRoutes = Router();
 
-transactionsRoutes.post("/deposit", 
+transactionsRoutes.post('/deposit', depositTransactionController.handle);
+transactionsRoutes.post('/deposit', depositTransactionController.handle);
+
+export default transactionsRoutes;
